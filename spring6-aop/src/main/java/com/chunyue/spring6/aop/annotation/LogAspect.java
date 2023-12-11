e1cb39cae8e754dcd7d167313e7ec958b4b6f2da
Chunyue Ma <cma01@wesleyan.edu>
2023-12-11 09:04:45 -0800

    Add @After notification

--- spring6-aop/src/main/java/com/chunyue/spring6/aop/annotation/LogAspect.java
+++ spring6-aop/src/main/java/com/chunyue/spring6/aop/annotation/LogAspect.java
@@ -1,5 +1,7 @@
 package com.chunyue.spring6.aop.annotation;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,6 +15,11 @@ public class LogAspect {
     // to cut in and when
     public void authentication() {
         System.out.println("Before any operation, doing necessary authentication!");
+    }
+
+    @After(value = "execution(* com.chunyue.spring6.aop.annotation.UserControllerImpl.*(..))")
+    public void afterMethod(JoinPoint joinPoint) {
+        System.out.println("Finished CRUD operation of user: " + joinPoint.getSignature().getName());
     }
 
 }
