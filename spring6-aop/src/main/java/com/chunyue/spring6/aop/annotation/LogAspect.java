fcf9c8055a960095a0abd170bdedd7150ae7dac1
Chunyue Ma <cma01@wesleyan.edu>
2023-12-12 06:53:40 -0800

    Use @Around to realize all other notifications

--- spring6-aop/src/main/java/com/chunyue/spring6/aop/annotation/LogAspect.java
+++ spring6-aop/src/main/java/com/chunyue/spring6/aop/annotation/LogAspect.java
@@ -1,8 +1,11 @@
 package com.chunyue.spring6.aop.annotation;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @Aspect
@@ -30,6 +33,24 @@ public class LogAspect {
                    throwing = "exception")
     public void afterThrowing(JoinPoint joinPoint, Throwable exception) {
         System.out.println("Exception encountered in " + joinPoint.getSignature().getName() + " with " + exception.getMessage());
+    }
+
+    @Around(value = "execution(* com.chunyue.spring6.aop.annotation.UserControllerImpl.*(..))")
+    public Object around(ProceedingJoinPoint joinPoint) {
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        String argsString = Arrays.toString(args);
+        Object res = null;
+        try {
+            System.out.println("@Around: before method execution");
+            res = joinPoint.proceed();
+            System.out.println("@Around: after method execution");
+        } catch (Throwable throwable) {
+            System.out.println("@Around: method exits with exception: " + throwable.getMessage());
+        } finally {
+            System.out.println("@Around: after method returned");
+        }
+        return res;
     }
 
 }
