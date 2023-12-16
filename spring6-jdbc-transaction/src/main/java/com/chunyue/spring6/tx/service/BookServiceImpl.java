a4c9e978504e107ab64ed86794f9499f85d1199d
Chunyue Ma <cma01@wesleyan.edu>
2023-12-16 09:27:43 -0800

    Add in transaction support

--- spring6-jdbc-transaction/src/main/java/com/chunyue/spring6/tx/service/BookServiceImpl.java
+++ spring6-jdbc-transaction/src/main/java/com/chunyue/spring6/tx/service/BookServiceImpl.java
@@ -3,6 +3,7 @@ package com.chunyue.spring6.tx.service;
 import com.chunyue.spring6.tx.dao.BookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookServiceImpl implements BookService {
@@ -10,6 +11,7 @@ public class BookServiceImpl implements BookService {
     @Autowired
     private BookDao bookDao;
 
+    @Transactional
     @Override
     public void purchaseBook(Integer bookId, Integer userId) {
         // get book price and stock based on bookId
